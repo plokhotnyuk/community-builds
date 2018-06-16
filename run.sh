64aa342c9da45404844bd4dda7bd0585629519af
Seth Tisue <seth@tisue.net>
2018-06-16 19:38:10 -0400

    freeze build for JVM testing
    reference run for the freeze was
    https://scala-ci.typesafe.com/view/scala-2.12.x/job/scala-2.12.x-integrate-community-build/3007/
    
    context is https://github.com/scala/community-builds/issues/733

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ export ORIGINAL_HOME=$HOME
 export HOME="$(pwd)"
 
 # Defaults
-scala_version_default="2.12.7-bin-5622f5d"  # June 11
+scala_version_default="2.12.6"
 scala_version="$scala_version_default"
 root_dir=$(pwd)
 config_dir="configs"
